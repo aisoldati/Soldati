49a06a7dec98cf1a1c8f3caeed0637c24451b2ae
aisoldati <agustin.soldati@gmail.com>
2022-09-12 00:22:55 -0300

    Corrigiendo errores

--- FrontEnd/src/app/service/auth.service.ts
+++ FrontEnd/src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'http://localhost:8080/auth/';
+  authURL = 'https://backendaisoldati.herokuapp.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
